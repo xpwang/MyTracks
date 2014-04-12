ccddfba72f95e5f284134acca5bc09adaaea98d7
xpwang <xpwang@iastate.edu>
2014-04-12 14:40:01 -0500

    Delect project
    Xiang Wang

--- gen/com/google/android/maps/mytracks/test/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.google.android.maps.mytracks.test;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
